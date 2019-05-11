323da96f4e4022775f632857ba361efe297ff493
Krishnasony <krishna.sony@talview.com>
2019-05-11 14:50:32 +0530

    Initial setup

---
+++ app/src/main/java/i/krishnasony/pratilipicontacts/ContactListActivity.kt
@@ -0,0 +1,12 @@
+package i.krishnasony.pratilipicontacts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.contact_list)
+    }
+}
